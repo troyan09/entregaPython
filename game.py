6dcdf267c5d1af6f4269adefe7c6d237a5583837
Alejandro Camacho <alecmacho2002@gmail.com>
2024-03-21 19:37:37 -0300

    corrijo el gub de caracter vacio

--- game.py
+++ game.py
@@ -11,27 +11,29 @@ print(f"Palabra: {word_displayed}")
 
 for i in range(max_attempts):
     letter = input("Ingresa una letra: ").lower()
-
-    if letter in guessed_letters:
-        print("Ya has intentado con esa letra. Intenta con otra.")
-        continue
-    guessed_letters.append(letter)
-    if letter in secret_word:
-        print("¡Bien hecho! La letra está en la palabra.")
-    else:
-        print("Lo siento, la letra no está en la palabra.")
-    letters = []
-    for letter in secret_word:
+    if letter != "":
         if letter in guessed_letters:
-            letters.append(letter)
+            print("Ya has intentado con esa letra. Intenta con otra.")
+            continue
+        guessed_letters.append(letter)
+        if letter in secret_word:
+            print("¡Bien hecho! La letra está en la palabra.")
         else:
-            letters.append("_")
-    word_displayed = "".join(letters)
-    print(f"Palabra: {word_displayed}")
- # Verificar si se ha adivinado la palabra completa
-    if word_displayed == secret_word:
-        print(f"¡Felicidades! Has adivinado la palabra secreta:{secret_word}")
-        break
+            print("Lo siento, la letra no está en la palabra.")
+            letters = []
+            for letter in secret_word:
+                if letter in guessed_letters:
+                    letters.append(letter)
+                else:
+                    letters.append("_")
+            word_displayed = "".join(letters)
+            print(f"Palabra: {word_displayed}")
+
+            if word_displayed == secret_word:
+                print(f"Felicidades, has adivinado la palabra secreta")
+                break
+    else:
+        print("Error! ingrese solo letras")
 else:
     print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
     print(f"La palabra secreta era: {secret_word}")
